3eec0139d77b16e58d204b2424268975befbe58b
Tachawin <Tachawin.test@gmail.com>
2020-03-31 00:09:27 +0700

    hello_bam

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 var express = require("express");
 var app = express();
-const connection = require("./connection/connection");
- 
+var bodyParser = require("body-parser");
+var session = require('express-session');
+var methodOverride = require("method-override");
+var expressSanitizer = require("express-sanitizer");
+
+//requring route
+var home = require("./routes/home");
+var auth = require("./routes/auth");
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(express.static("JS"));
- 
-app.get('/', function (req, res) {
-        res.render("home");
-        })
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 
-app.get('/home', function (req, res) {
-    res.render("home");
-    })
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
-app.get('/login', function (req, res) {
-    res.render("login");
-    })
+
 
 app.get('/cinema', function (req, res) {
     res.render("cinema");
@@ -28,8 +34,13 @@ app.get('/movie', function (req, res) {
 app.get('/promotion', function (req, res) {
     res.render("promotion");
     })
+app.use(function(req,res,next){
+    res.locals.currentUser = req.session.email;
+    next();
+    })
+app.use("/",home);
+app.use("/",auth);
 
- 
  var server = app.listen(3000,function(){
  console.log("We have started our server on port 3000");
 });
---
+++ routes/auth.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var router = express.Router();
+const connection = require("../connection/connection");
+
+router.get('/login', function (req, res) {
+    res.render("login",{error:''});
+    })
+
+//handling user sign in
+router.post("/login", function (req, res) {
+    var email = req.body.email;
+    var password = req.body.pass;
+    if (email && password) {
+        connection.query('SELECT * FROM Member WHERE email = ? AND password = ?', [email, password], function (error, results, fields) {
+            console.log(results);
+            if (results.length > 0) {
+                req.session.email = email;
+                //req.flash("success","welcome to yelp camp" + req.session.username);
+                res.redirect('/');
+            } else {
+                res.render("login",{error:1});
+            }
+            res.end();
+        });
+    } else {
+        res.send('Please enter Username and Password!');
+        res.end();
+    }
+});
+
+//logout routes
+router.get("/logout", function (req, res) {
+    req.session.destroy();
+    res.redirect("/");
+});
+
+module.exports = router;
